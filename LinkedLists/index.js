89201b353aee835d9a3de04c540fdc3999cc48d5
Adrian Viquez <aviquez96@gmail.com>
2019-02-07 14:09:35 -0800

    Implementation of getFirst and getLast methods in LinkedList

--- LinkedLists/index.js
+++ LinkedLists/index.js
@@ -3,158 +3,54 @@
 // See 'directions' document
 
 class Node {
-  constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
-  }
+    // When you do equivalences in function signatrues in javascript, they apply only when the client hasn't specified a value (a default value)
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
 
-  insertFirst(data) {
-    this.head = new Node(data, this.head);
-  }
-
-  size() {
-    let counter = 0;
-    let node = this.head;
-
-    while (node) {
-      counter++;
-      node = node.next;
+    constructor() {
+        this.head = null;
     }
 
-    return counter;
-  }
-
-  getFirst() {
-    return this.head;
-  }
-
-  getLast() {
-    if (!this.head) {
-      return null;
+    insertFirst(data) {
+        let newNode = new Node(data, this.head);
+        this.head = newNode;
     }
 
-    let node = this.head;
-    while (node) {
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
-  }
+    size () {
+        let listSize = 0;
+        let traversingNode = this.head; 
 
-  clear() {
-    this.head = null;
-  }
+        while (traversingNode) {
+            listSize++;
+            traversingNode = traversingNode.next; 
+        }
 
-  removeFirst() {
-    if (!this.head) {
-      return;
+        return listSize;
     }
 
-    this.head = this.head.next;
-  }
-
-  removeLast() {
-    if (!this.head) {
-      return;
+    getFirst() {
+        return this.head;
     }
 
-    if (!this.head.next) {
-      this.head = null;
-      return;
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let traversingNode = this.head;
+
+        while (traversingNode) {
+            if (traversingNode.next == null) {
+                return traversingNode;
+            }
+            traversingNode = traversingNode.next;
+        }
     }
-
-    let previous = this.head;
-    let node = this.head.next;
-    while (node.next) {
-      previous = node;
-      node = node.next;
-    }
-    previous.next = null;
-  }
-
-  insertLast(data) {
-    const last = this.getLast();
-
-    if (last) {
-      // There are some existing nodes in our chain
-      last.next = new Node(data);
-    } else {
-      // The chain is empty!
-      this.head = new Node(data);
-    }
-  }
-
-  getAt(index) {
-    let counter = 0;
-    let node = this.head;
-    while (node) {
-      if (counter === index) {
-        return node;
-      }
-
-      counter++;
-      node = node.next;
-    }
-    return null;
-  }
-
-  removeAt(index) {
-    if (!this.head) {
-      return;
-    }
-
-    if (index === 0) {
-      this.head = this.head.next;
-      return;
-    }
-
-    const previous = this.getAt(index - 1);
-    if (!previous || !previous.next) {
-      return;
-    }
-    previous.next = previous.next.next;
-  }
-
-  insertAt(data, index) {
-    if (!this.head) {
-      this.head = new Node(data);
-      return;
-    }
-
-    if (index === 0) {
-      this.head = new Node(data, this.head);
-      return;
-    }
-
-    const previous = this.getAt(index - 1) || this.getLast();
-    const node = new Node(data, previous.next);
-    previous.next = node;
-  }
-
-  forEach(fn) {
-    let node = this.head;
-    let counter = 0;
-    while (node) {
-      fn(node, counter);
-      node = node.next;
-      counter++;
-    }
-  }
-
-  *[Symbol.iterator]() {
-    let node = this.head;
-    while (node) {
-      yield node;
-      node = node.next;
-    }
-  }
 }
 
 module.exports = { Node, LinkedList };
