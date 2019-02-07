a15d6b401f3f852ca6331e352baba1fa65c3ed38
Adrian Viquez <aviquez96@gmail.com>
2019-02-06 22:02:42 -0800

    solutions to fib with memoization

--- Fib/index.js
+++ Fib/index.js
@@ -9,23 +9,23 @@
 //   fib(4) === 3
 
 // SOLUTION 1
-function fib(n) {
-    let fibNum = 0;
-    let previousNum1 = 0;
-    let previousNum2 = 1;
+// function fib(n) {
+//     let fibNum = 0;
+//     let previousNum1 = 0;
+//     let previousNum2 = 1;
     
-    for (let i = 0; i < n; i++) {
-        fibNum = previousNum1 + previousNum2;
-        previousNum2 = previousNum1;
-        previousNum1 = fibNum;
-    }
+//     for (let i = 0; i < n; i++) {
+//         fibNum = previousNum1 + previousNum2;
+//         previousNum2 = previousNum1;
+//         previousNum1 = fibNum;
+//     }
 
-    return fibNum
+//     return fibNum
 
-}
+// }
 
 // SOLUTION 2 (recursion)
-function fib(n) {
+function slowFib(n) {
     // base case
     if (n <2) {
         return n;
@@ -34,5 +34,22 @@ function fib(n) {
     return fib(n-1) + fib(n-2);  
 
 }
+
+function memoize (fn) {
+    let cache = {};
+    return function(...args) {
+        //Have we ever called this function with these set of arguments? If so, return it!
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        
+        return result;
+    }
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
